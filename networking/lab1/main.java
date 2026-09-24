db8078846c4ac0e9bf07820703793f051205a672
tanishq Chavan <stapdr7eam@gmail.com>
2026-09-24 16:37:56 +0530

    feat(network): inspect interface state and format MAC addresses

--- networking/lab1/main.java
+++ networking/lab1/main.java
@@ -1,8 +1,10 @@
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Enumeration;
 import java.util.List;
+import java.util.Arrays;
 
 
 public class main {
@@ -21,7 +23,25 @@ public class main {
         System.out.println("found " + interfaceList.size() + " network interface.");
 
         for (NetworkInterface ni : interfaceList) {
-            System.out.println(ni.getName());
+            if (ni.isUp()) {
+                System.out.println(ni.getName());
+                byte[] mac = ni.getHardwareAddress();
+                if (mac == null) {
+                    System.out.println(ni.getName() + " as no MAC address.");
+                    continue;
+                }
+                for (int i = 0; i < mac.length; i++) {
+                    if (i > 0) {
+                        System.out.print(":");
+                    }
+
+                    System.out.printf("%02x", mac[i] & 0xff);
+
+                }
+                System.out.println();
+            }
+            Enumeration<InetAddress> addresses = ni.getInetAddresses();
+
         }
     }
 }
